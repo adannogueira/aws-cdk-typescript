84868b0714d906a0805ee6216c6208f1d23fc92d
Adan Nogueira <adan.nogueira@gmail.com>
2023-02-01 21:14:28 -0300

    feat: add environment variables to  CdkTypescriptStack

--- bin/cdk-typescript.ts
+++ bin/cdk-typescript.ts
@@ -1,6 +1,14 @@
 #!/usr/bin/env node
+import * as dotenv from 'dotenv'
 import * as cdk from 'aws-cdk-lib';
 import { CdkTypescriptStack } from '../lib/cdk-typescript-stack';
 
+dotenv.config()
+
 const app = new cdk.App();
-new CdkTypescriptStack(app, 'CdkTypescriptStack');
+new CdkTypescriptStack(app, 'CdkTypescriptStack', {
+  env: {
+    account: process.env.ACCOUNT,
+    region: process.env.REGION
+  },
+});
