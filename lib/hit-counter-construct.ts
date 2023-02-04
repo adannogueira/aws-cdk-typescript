e3943b1d1c37da93fb37c68331f877cc7175fb27
Adan Nogueira <adan.nogueira@gmail.com>
2023-02-04 16:19:35 -0300

    feat: ensure HitCounterConstruct passes env variables to lambda function

--- lib/hit-counter-construct.ts
+++ lib/hit-counter-construct.ts
@@ -1,5 +1,7 @@
-import { IFunction } from 'aws-cdk-lib/aws-lambda';
+import { IFunction, Runtime } from 'aws-cdk-lib/aws-lambda';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { Construct } from 'constructs';
+import { join } from 'path';
 import { Database } from '../resources/database';
 
 export interface HitCounterProps {
@@ -7,9 +9,20 @@ export interface HitCounterProps {
 }
 
 export class HitCounterConstruct extends Construct {
-  constructor (scope: Construct, id: string, props?: HitCounterProps) {
+  public readonly handler: NodejsFunction;
+
+  constructor (scope: Construct, id: string, props: HitCounterProps) {
     super(scope, id);
 
-    const table = new Database(this, 'Database');
+    const table = new Database(this, 'Database').db;
+    this.handler = new NodejsFunction(this, 'HitCounterHandler', {
+      runtime: Runtime.NODEJS_14_X,
+      entry: join(__dirname, `/../lambda/hit-counter.ts`),
+      handler: 'hitCounter',
+      environment: {
+        DOWNSTREAM_FUNCTION_NAME: props.downstream.functionName,
+        HITS_TABLE_NAME: table.tableName
+      }
+    })
   }
 }
--- test/lib/hit-counter-construct.test.ts
+++ test/lib/hit-counter-construct.test.ts
@@ -1,12 +1,46 @@
 import { Stack } from 'aws-cdk-lib';
-import { Template } from 'aws-cdk-lib/assertions';
+import { Capture, Template } from 'aws-cdk-lib/assertions';
+import { Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda';
 import { HitCounterConstruct } from '../../lib/hit-counter-construct';
 
 describe('HitCounterConstruct', () => {
   it('Should create DynamoDB Table', () => {
     const stack = new Stack();
-    new HitCounterConstruct(stack, 'TestConstruct');
+    new HitCounterConstruct(stack, 'TestConstruct', {
+      downstream: new Function(stack, 'TestFunction', {
+        runtime: Runtime.NODEJS_14_X,
+        code: Code.fromAsset('lambda'),
+        handler: 'hello'
+      })
+    });
     const template = Template.fromStack(stack);
     template.resourceCountIs('AWS::DynamoDB::Table', 1);
-  })
-})
+  });
+
+  it('Should pass environment variables to lambda function', () => {
+    const stack = new Stack();
+    new HitCounterConstruct(stack, 'TestConstruct', {
+      downstream: new Function(stack, 'TestFunction', {
+        runtime: Runtime.NODEJS_14_X,
+        code: Code.fromAsset('lambda'),
+        handler: 'hello'
+      })
+    });
+    const template = Template.fromStack(stack);
+    const envCapture = new Capture();
+    template.hasResourceProperties('AWS::Lambda::Function', {
+      Environment: envCapture
+    });
+    expect(envCapture.asObject()).toEqual({
+      Variables: {
+        AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+        DOWNSTREAM_FUNCTION_NAME: {
+          Ref: 'TestFunction22AD90FC'
+        },
+        HITS_TABLE_NAME: {
+          Ref: 'TestConstructDatabaseHitsE0602B24'
+        }
+      }
+    });
+  });
+});
