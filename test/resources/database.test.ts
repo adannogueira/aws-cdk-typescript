d749fd4471015b3d2c4639489bee9085617187a8
Adan Nogueira <adan.nogueira@gmail.com>
2023-02-03 22:10:13 -0300

    feat: ensure Database creates a DynamoDB table

--- test/resources/database.test.ts
+++ test/resources/database.test.ts
@@ -1,10 +1,18 @@
 import { Stack } from 'aws-cdk-lib';
-import { Database } from '../../resources/database'
+import { Template } from 'aws-cdk-lib/assertions';
+import { Database } from '../../resources/database';
 
 describe('Database', () => {
   it('Should be correctly instantiated', () => {
     const stack = new Stack();
     const database = new Database(stack, 'MyDatabase');
     expect(database).toBeInstanceOf(Object);
+  });
+
+  it('Should create a DynamoDB table', () => {
+    const stack = new Stack();
+    new Database(stack, 'MyDatabase');
+    const template = Template.fromStack(stack);
+    template.resourceCountIs('AWS::DynamoDB::Table', 1);
   });
 });
