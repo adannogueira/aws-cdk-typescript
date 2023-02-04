0ac6bd525e0c91cb5929b67968865ddeadcbfa19
Adan Nogueira <adan.nogueira@gmail.com>
2023-02-04 17:47:37 -0300

    refactor: separate concerns, improve readability

--- lib/hit-counter-construct.ts
+++ lib/hit-counter-construct.ts
@@ -1,3 +1,4 @@
+import { Table } from 'aws-cdk-lib/aws-dynamodb';
 import { IFunction, Runtime } from 'aws-cdk-lib/aws-lambda';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { Construct } from 'constructs';
@@ -11,19 +12,30 @@ export interface HitCounterProps {
 export class HitCounterConstruct extends Construct {
   public readonly handler: NodejsFunction;
 
-  constructor (scope: Construct, id: string, props: HitCounterProps) {
+  constructor (
+    scope: Construct,
+    id: string,
+    private props: HitCounterProps
+  ) {
     super(scope, id);
+    const table = this.initializeDatabase();
+    this.handler = this.initializeLambda(table);
+    table.grantReadWriteData(this.handler);
+  }
 
-    const table = new Database(this, 'Database').db;
-    this.handler = new NodejsFunction(this, 'HitCounterHandler', {
+  private initializeDatabase(): Table {
+    return new Database(this, 'Database').db;
+  }
+
+  private initializeLambda(table: Table): NodejsFunction {
+    return new NodejsFunction(this, 'HitCounterHandler', {
       runtime: Runtime.NODEJS_14_X,
       entry: join(__dirname, `/../lambda/hit-counter.ts`),
       handler: 'hitCounter',
       environment: {
-        DOWNSTREAM_FUNCTION_NAME: props.downstream.functionName,
+        DOWNSTREAM_FUNCTION_NAME: this.props.downstream.functionName,
         HITS_TABLE_NAME: table.tableName
       }
-    })
-    table.grantReadWriteData(this.handler);
+    });
   }
 }
