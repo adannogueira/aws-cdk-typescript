6e5ca34e29664b287ec6aa0ca3ef01629ab224d9
Adan Nogueira <adan.nogueira@gmail.com>
2023-02-04 19:10:34 -0300

    fix: get table name from environment

--- resources/database.ts
+++ resources/database.ts
@@ -18,7 +18,7 @@ export class Database extends Construct {
 export const increaseDbCountFor = async (endpoint: string): Promise<void> => {
   const dynamo = new DynamoDB();
   await dynamo.updateItem({
-    TableName: 'some_table',
+    TableName: process.env.HITS_TABLE_NAME!,
     Key: { path: { S: endpoint } },
     UpdateExpression: 'ADD hits :incr',
     ExpressionAttributeValues: { ':incr' : { N: '1' } }
